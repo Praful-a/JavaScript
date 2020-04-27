b23798aee0c6ab500e7e45ee7a2750cb069fadf3
Praful-a <prafuldubey27@gmail.com>
2020-04-27 22:33:34 +0530

    Loops and iteration and completed coding challenge no 5.

---
+++ 1-JS_Basics/Loops_and_iteration/script.js
@@ -0,0 +1,39 @@
+/*******************
+* Loops and iteration
+*/
+/*
+// For loop
+for (var i = 1; i < 20; i+=2) {
+  console.log(i);
+}
+
+var john = ['John', 'Smith', 1990, 'designer', false, 'blue'];
+for (var i = 0; i < john.length; i++) {
+  console.log(john[i]);
+}
+
+// While Loop
+var i = 0;
+while(i < john.length) {
+  console.log(john[i]);
+  i++;
+}
+*/
+
+// continue and break statements
+
+var john = ['John', 'Smith', 1990, 'designer', false, 'blue'];
+for (var i = 0; i < john.length; i++) {
+  if (typeof john[i] !== 'string') continue;
+  console.log(john[i]);
+}
+
+for (var i = 0; i < john.length; i++){
+  if (typeof john[i] !== 'string') break;
+  console.log(john[i]);
+}
+
+// Looping backwards
+for (var i = john.length - 1; i >= 0; i--){
+  console.log(john[i]);
+}
