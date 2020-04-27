71e6799c60cfed595da6addc1b857acaf1a2b6a1
Praful-a <prafuldubey27@gmail.com>
2020-04-27 21:38:12 +0530

    Object and properties in js.

---
+++ 1-JS_Basics/Objects_and_properties/script.js
@@ -0,0 +1,27 @@
+/*************************
+* Objects and properties
+*/
+
+var john = {
+  firstName: 'John',
+  lastName: 'Smith',
+  birthYear: 1990,
+  family: ['Jane', 'Mark', 'Bob', 'Emily'],
+  job: 'teacher',
+  isMarried: false
+};
+console.log(john.firstName);
+console.log(john.firstName);
+var x = 'birthYear';
+console.log(john[x]);
+
+john.job = 'designer';
+john['isMarried'] = true;
+console.log(john);
+
+// new Object syntax
+var jane = new Object();
+jane.firstName = 'Jane';
+jane.birthYear = 1969;
+jane['lastName'] = 'Smith';
+console.log(jane);
