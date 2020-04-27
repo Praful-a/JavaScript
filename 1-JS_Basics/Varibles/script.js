6abf20410a6ba547153b11c8d7ec89c5395e18ed
Praful-a <prafuldubey27@gmail.com>
2020-04-27 13:51:50 +0530

    Variables in js.

---
+++ 1-JS_Basics/Varibles/script.js
@@ -0,0 +1,24 @@
+// console.log('Hello World!!!');
+var firstName = 'John';
+console.log(firstName);
+
+var lastName = 'Smith';
+var age = 28;
+
+var fullAge = true;
+console.log(fullAge);
+
+var job;
+console.log(job);
+job = 'Teacher';
+console.log(job);
+
+// You can not define numbers as a variable in js
+// var 3years = 3; like this
+var _3years = 4; //this is the right syntax
+// or you can not define any symbol in js
+// var john/Mark = 'John and Mark' like this
+// you have to type.
+johnMark = 'John and Mark';
+// or you can not use JavaScript reserved words as a variables.
+// var if = 23; or var function = 22; like this.
