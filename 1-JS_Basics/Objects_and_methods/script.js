8d930f16bfd3e98fe34066bf7136a8db147844c4
Praful-a <prafuldubey27@gmail.com>
2020-04-27 21:50:22 +0530

    objects and methods in javascript.

---
+++ 1-JS_Basics/Objects_and_methods/script.js
@@ -0,0 +1,18 @@
+/*************************
+* Objects and methods
+*/
+
+var john = {
+  firstName: 'John',
+  lastName: 'Smith',
+  birthYear: 1992,
+  family: ['Jane', 'Mark', 'Bob', 'Emily'],
+  job: 'teacher',
+  isMarried: false,
+  calcAge: function() {
+    this.age = 2018 - this.birthYear;
+  }
+};
+
+john.calcAge();
+console.log(john);
