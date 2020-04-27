ecfa9d18ab41787e73b66844675bf389176fe518
Praful-a <prafuldubey27@gmail.com>
2020-04-27 14:17:24 +0530

    Basic operators uses in js.

---
+++ 1-JS_Basics/Basic_Operators/script.js
@@ -0,0 +1,27 @@
+/***********************
+* Basic Operators
+*/
+var year, yearJohn, yearMark
+year = 2018;
+ageJohn = 28;
+ageMark = 33;
+// Math Operators
+yearJohn = year - ageJohn;
+yearMark = year - ageMark;
+console.log(yearJohn);
+console.log(yearMark);
+
+console.log(year + 2);
+console.log(year * 2);
+console.log(year / 2);
+
+// Logical operators
+var johnOlder = ageJohn > ageMark;
+console.log(johnOlder);
+
+// typeof operator
+console.log(typeof johnOlder);
+console.log(typeof ageJohn);
+console.log(typeof 'Mark is older than John');
+var x;
+console.log(typeof x);
