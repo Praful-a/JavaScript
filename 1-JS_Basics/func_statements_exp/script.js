90874b434d8881400b9fc9e019090e0293f9b916
Praful-a <prafuldubey27@gmail.com>
2020-04-27 16:51:30 +0530

    Function Statements and Expressions.

---
+++ 1-JS_Basics/func_statements_exp/script.js
@@ -0,0 +1,24 @@
+/*******************
+* Function Statements and Expressions
+*/
+
+// Function declaration
+// Function whatDoYouDo(job, firstName) {}
+
+// Function expression
+var whatDoYouDo = function(job, firstName) {
+  switch(job) {
+    case 'teacher' :
+        return firstName + ' teaches kids how to code';
+    case 'driver':
+        return firstName + ' drives a cab in Lisbon';
+    case 'designer':
+        return firstName + ' designs beautiful websites.';
+    default:
+        return firstName + ' does something else';
+  }
+}
+
+console.log(whatDoYouDo('teacher', 'John'));
+console.log(whatDoYouDo('designer', 'Jane'));
+console.log(whatDoYouDo('retired', 'Mark'));
