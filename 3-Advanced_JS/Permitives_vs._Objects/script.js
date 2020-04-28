2043714b12f46e53a8c2186f503df4779b7b522b
Praful-a <prafuldubey27@gmail.com>
2020-04-28 22:25:51 +0530

    Functions Returning Functions.

--- 3-Advanced_JS/Permitives_vs._Objects/script.js
+++ 3-Advanced_JS/Permitives_vs._Objects/script.js
@@ -39,7 +39,7 @@ console.log(obj.city);
 
 ///////////////////////////////////
 // Lecture: Passing functions as arguments
-
+/*
 var years = [1990, 1965, 1937, 2005, 1998];
 function arrayCalc(arr, fn) {
   var arrRes = [];
@@ -72,3 +72,34 @@ var rates = arrayCalc(ages, maxHeartRate);
 console.log(ages);
 console.log(fullAges);
 console.log(rates);
+*/
+
+///////////////////////////////
+// Lecture: Functions returning Functions
+
+function interviewQuestion(job) {
+  if (job === 'designer') {
+    return function(name) {
+      console.log(name + ', can you please explain what UX design is?');
+    }
+  } else if (job === 'teacher') {
+    return function(name) {
+      console.log('What subject do you teach, ' + name + '?');
+    }
+  } else {
+    return function(name) {
+      console.log('Hello ' + name + ', what do you do?');
+    }
+  }
+}
+
+var teacherQuestion = interviewQuestion('teacher');
+var designerQuestion = interviewQuestion('designer');
+
+teacherQuestion('John');
+designerQuestion('John');
+designerQuestion('jane');
+designerQuestion('Mark');
+designerQuestion('Mike');
+
+interviewQuestion('teacher')('Mark');
