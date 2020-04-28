38fb0214ffc3dbd6efc429a04527c16917f42bed
Praful-a <prafuldubey27@gmail.com>
2020-04-28 22:15:41 +0530

    Class Functions Passing Functions as Arguments.

--- 3-Advanced_JS/Permitives_vs._Objects/script.js
+++ 3-Advanced_JS/Permitives_vs._Objects/script.js
@@ -1,6 +1,7 @@
 // Primitives vs objects
 
 // Primitives
+/*
 var a = 23;
 var b = a;
 a = 46;
@@ -34,3 +35,40 @@ change(age, obj)
 
 console.log(age);
 console.log(obj.city);
+*/
+
+///////////////////////////////////
+// Lecture: Passing functions as arguments
+
+var years = [1990, 1965, 1937, 2005, 1998];
+function arrayCalc(arr, fn) {
+  var arrRes = [];
+  for (var i = 0; i < arr.length; i++) {
+    arrRes.push(fn(arr[i]));
+  }
+  return arrRes;
+}
+
+function calculateAge(el) {
+  return 2016 - el;
+}
+
+function isFullAge(el) {
+  return el >= 18;
+}
+
+function maxHeartRate(el) {
+  if (el >= 18 && el <= 81) {
+    return Math.round(206.9 - (0.67 * el));
+  } else {
+    return -1
+  }
+}
+
+var ages = arrayCalc(years, calculateAge);
+var fullAges = arrayCalc(ages, isFullAge);
+var rates = arrayCalc(ages, maxHeartRate);
+
+console.log(ages);
+console.log(fullAges);
+console.log(rates);
