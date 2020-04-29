b84a3d890fc913155a9fb90eeffb323f23c2e6b1
Praful-a <prafuldubey27@gmail.com>
2020-04-29 16:56:41 +0530

    Updated challenge 7

--- CODING_CHALLENGE/COD_CH-6/script.js
+++ CODING_CHALLENGE/COD_CH-6/script.js
@@ -16,7 +16,7 @@ c) correct answer (I would use a number for this)
 6. Check if the answer is correct and print to the console whether the answer is correct ot nor (Hint: write another method for this).
 7. Suppose this code would be a plugin for other programmers to use in their code. So make sure that all your code is private and doesn't interfere with the other programmers code (Hint: we learned a special technique to do exactly that).
 */
-/*
+
 (function() {
   function Question(question, answers, correct) {
     this.question = question;
