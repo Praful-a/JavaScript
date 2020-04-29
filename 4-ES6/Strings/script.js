d94ab4b9abdc7b24cfe134df65e48ba5c28ab12f
Praful-a <prafuldubey27@gmail.com>
2020-04-29 18:03:21 +0530

    Strings in ES6.

---
+++ 4-ES6/Strings/script.js
@@ -0,0 +1,23 @@
+///////////////////////////////
+// Lecture: Strings
+
+let firstName = 'John';
+let lastName = 'Smith';
+const yearOfBirth= 1990;
+
+function calcAge(year) {
+  return 2016 - year;
+}
+
+// ES5
+console.log('This is ' + firstName + ' ' + lastName + '. He was born in ' + yearOfBirth + '. Today, he is ' + calcAge(yearOfBirth) + ' years old.');
+
+// ES6
+console.log(`This is ${firstName} ${lastName}. He was born in ${yearOfBirth}. Today, he is ${calcAge(yearOfBirth)} years old.`);
+
+const n = `${firstName} ${lastName}`;
+console.log(n.startsWith('J'));
+console.log(n.endsWith('th'));
+console.log(n.includes(' '));
+console.log(n.includes('oh'));
+console.log(`${firstName} `.repeat(5));
