1306c6f7f87b11f252ffbc41e2b3bab9c685550e
Praful-a <prafuldubey27@gmail.com>
2020-04-30 15:14:08 +0530

    Default parameters in javascript.

--- 4-ES6/Rest_parameters/script.js
+++ 4-ES6/Rest_parameters/script.js
@@ -2,7 +2,7 @@
 // Lecture: Rest parameters
 
 // ES5
-/*
+
 function isFullAge5() {
   // console.log(arguments);
   var argsArr = Array.prototype.slice.call(arguments)
@@ -10,16 +10,16 @@ function isFullAge5() {
     console.log(2016 - cur >= 18);
   });
 }
-*/
-// isFullAge5(1990, 1999, 1965);
-// isFullAge5(1990, 1999, 1965, 2016, 1987);
+
+isFullAge5(1990, 1999, 1965);
+isFullAge5(1990, 1999, 1965, 2016, 1987);
 
 //ES6
-/*
+
 function isFullAge6(...years) {
   years.forEach(cur => console.log((2016 - cur) >= 18));
 }
-*/
+
 
 function isFullAge5(limit) {
   console.log(arguments);
